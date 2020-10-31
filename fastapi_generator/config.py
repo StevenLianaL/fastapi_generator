253d9a7421717863f44fe86585b20d6d5da312ed
wangzhou <wangzhou8284@outlook.com>
2020-10-31 10:58:37 +0800

    fix .ignore

--- fastapi_generator/config.py
+++ fastapi_generator/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class Settings:
     top_dirs = ('app', 'log', 'test')
-    top_files = ('README.md', 'requirements.txt', ' .gitignore')
+    top_files = ('README.md', 'requirements.txt', '.gitignore')
     app_dirs = ('sub_apps',)
     app_files = ('__init__', 'config', 'data', 'db', 'models', 'interface', 'main')
 
