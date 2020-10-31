10d2001babdfbe9f9b92851c7551bb7792876606
wangzhou <wangzhou8284@outlook.com>
2020-10-31 10:57:18 +0800

    declare main app class

--- fastapi_generator/main.py
+++ fastapi_generator/main.py
@@ -5,23 +5,29 @@ import plac
 from fastapi_generator.config import Settings
 
 
-def gen_main_app():
-    # create folders
-    for folder in Settings.top_dirs:
-        folder = Path(folder)
-        if not folder.exists():
-            folder.mkdir()
+class MainApp:
+    @staticmethod
+    def create_app_files():
+        # create folders
+        for folder in Settings.top_dirs:
+            folder = Path(folder)
+            if not folder.exists():
+                folder.mkdir()
 
-    # create app files
-    for file in Settings.app_files:
-        Path(Settings.root_dir, file).with_suffix('.py').touch()
-    for app_dir in Settings.app_dirs:
-        Path(Settings.root_dir, app_dir).mkdir()
+        # create app files
+        for file in Settings.app_files:
+            Path(Settings.root_dir, file).with_suffix('.py').touch()
+        for app_dir in Settings.app_dirs:
+            Path(Settings.root_dir, app_dir).mkdir()
 
-    # create top files
-    for file in Settings.top_files:
-        Path(file).touch()
+        # create top files
+        for file in Settings.top_files:
+            Path(file).touch()
+
+    def create_app(self):
+        self.create_app_files()
 
 
 if __name__ == '__main__':
-    plac.call(gen_main_app)
+    app = MainApp()
+    plac.call(app.create_app)
