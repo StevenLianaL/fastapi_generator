65f6dbf847ba6c909062013a6101f1349598ab8f
wangzhou <wangzhou8284@outlook.com>
2020-10-31 10:48:53 +0800

    add config.py

---
+++ fastapi_generator/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+class Settings:
+    top_dirs = ('app', 'log', 'test')
+    top_files = ('README.md', 'requirements.txt', ' .gitignore')
+    app_dirs = ('sub_apps',)
+    app_files = ('__init__', 'config', 'data', 'db', 'models', 'interface', 'main')
+
+    root_dir = Path('app')
--- fastapi_generator/main.py
+++ fastapi_generator/main.py
@@ -2,27 +2,24 @@ from pathlib import Path
 
 import plac
 
+from fastapi_generator.config import Settings
+
 
 def gen_main_app():
     # create folders
-    folders = ['app', 'log', 'test']
-    for folder in folders:
+    for folder in Settings.top_dirs:
         folder = Path(folder)
         if not folder.exists():
             folder.mkdir()
-    root_dir = Path('app')
 
     # create app files
-    app_files = ('__init__', 'config', 'data', 'db', 'models', 'interface', 'main')
-    app_dirs = ('sub_apps',)
-    for file in app_files:
-        Path(root_dir, file).with_suffix('.py').touch()
-    for app_dir in app_dirs:
-        Path(root_dir, app_dir).mkdir()
+    for file in Settings.app_files:
+        Path(Settings.root_dir, file).with_suffix('.py').touch()
+    for app_dir in Settings.app_dirs:
+        Path(Settings.root_dir, app_dir).mkdir()
 
     # create top files
-    top_files = ('README.md', 'requirements.txt', ' .gitignore')
-    for file in top_files:
+    for file in Settings.top_files:
         Path(file).touch()
 
 
