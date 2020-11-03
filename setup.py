3a66b98ae9b18e66f5ed136f568c81f1cca9a77f
wangzhou <wangzhou8284@outlook.com>
2020-11-03 11:14:43 +0800

    for fix templates not build

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with Path("README.md").open("r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="fastapi_generator",
-    version="0.3",
+    version="0.3.1",
     author="Steven Wang",
     author_email="wangzhou8284@outlook.com",
     description="Used to quickly generate fastapi project.",
