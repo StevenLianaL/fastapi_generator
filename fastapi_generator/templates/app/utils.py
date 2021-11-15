8f5e09fcb6021eb95655ed97620d8922aaac5998
wz <wangzhou8284@gmail.com>
2021-11-15 08:36:07 +0800

    update utils

--- fastapi_generator/templates/app/utils.py
+++ fastapi_generator/templates/app/utils.py
@@ -1,6 +1,15 @@
 import importlib
+from dataclasses import dataclass
+from typing import Iterator
 
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends, Header
+# from redis import StrictRedis
+from starlette.middleware.cors import CORSMiddleware
+# from tortoise.exceptions import DoesNotExist
+
+from app.config import project
+# from app.middle import RecordMiddleware
+from app.sub_apps.auth.depends import AuthHeaders
 
 
 def load_app(sub_app_name: str) -> FastAPI:
@@ -22,3 +31,48 @@ def load_api(sub_app_name: str, api_name: str) -> APIRouter:
             return var
     else:
         raise Exception('Not found APIRouter.')
+
+
+@dataclass
+class AuthHeaders:
+    jwt: str = Header('', description="pass jwt in header")
+
+
+@dataclass
+class Appbuilder:
+    app_name: str
+
+    def __post_init__(self):
+        self.app = FastAPI(dependencies=[Depends(AuthHeaders)], title=self.app_name)
+        self.build_app()
+
+    def build_app(self) -> FastAPI:
+        self._add_middlewares()
+        # self._add_redis()
+        self._add_routers()
+        return self.app
+
+    def _add_middlewares(self):
+        self.app.add_middleware(CORSMiddleware,
+                                allow_origins='*',
+                                allow_methods=['*'],
+                                allow_headers=['*'],
+                                allow_credentials=True)
+
+    # def _add_redis(self):
+    #     self.app.state.redis = StrictRedis(
+    #         host=project.REDIS_HOST,
+    #         db=project.REDIS_DB,
+    #         decode_responses=True
+    #     )
+
+    def _add_routers(self):
+        for router_file in self._count_router_files():
+            api_name = router_file.stem.replace('_api', '')
+            if api_name != "__pycache__":
+                api = load_api(sub_app_name=self.app_name, api_name=api_name)
+                self.app.include_router(router=api, prefix=f"/{api_name}", tags=[api_name])
+
+    def _count_router_files(self) -> Iterator:
+        router_dir = project.SUB_APP_DIR / self.app_name / 'router'
+        return router_dir.iterdir()
